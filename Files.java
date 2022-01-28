7bb3eb7d334a048d4905740e216f2d881c6f7e32
aishwaryagobannavar5 <43140576+aishwaryagobannavar5@users.noreply.github.com>
2022-01-28 15:49:49 +0530

    Add files via upload

---
+++ Files.java
@@ -0,0 +1,26 @@
+package files;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Files {
+   public static void main( String[] args ) {
+
+      try {
+         String data = " Tutorials Point is a best website in the world";
+         File f1 = new File("C:\\Users\\Hp\\Documents");
+         if(!f1.exists()) {
+            f1.createNewFile();
+         }
+
+         FileWriter fileWritter = new FileWriter(f1.getName(),true);
+         BufferedWriter bw = new BufferedWriter(fileWritter);
+         bw.write(data);
+         bw.close();
+         System.out.println("Done");
+      } catch(IOException e){
+         e.printStackTrace();
+      }
+   }
+}
